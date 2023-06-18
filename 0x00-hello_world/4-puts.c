d0b53e9afdaa11ed047cb9170622a4cb54932874
favourella11 <favourella9@gmail.com>
2023-06-17 18:11:04 -0700

    hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main Entry point 
+ *
+ * Return Answer 0 (Success) 
+ */ 
+int main(void) 
+{ 
+	print("\"programming is like building a multilingual puzzle");
+	return (0); 
+} 
+
