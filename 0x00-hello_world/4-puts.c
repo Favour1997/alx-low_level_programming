7f835507c236e6cccbea52edfbe53b328361a701
favourella11 <favourella9@gmail.com>
2023-06-19 10:15:37 -0700

    hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,6 @@
  */ 
 int main(void) 
 { 
-	print("\"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0); 
 } 
-
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,17 @@
 #include<stdio.h>
+gcc 6-size. c -m32 -o size32 
 /**
  * main Entry point 
  *
  * Return Answer 0 (Success)
  */ 
 int main(void) 
-	print("Size of a char. %d byte(s)\n", sizeof(char));
-	print("Size of an int. %d byte(s)\n", sizeof(int));
-	print("Size of a long int. %d byte(s)\n", sizeof(long int));
-	print("Size of a lon long int. %d byte(s)\n", sizeof(long long int));
-	print("Size of a float. %d byte(s)\n", sizeof(float)); 
+{	
+	print("Size of a char. 1 byte(s)\n", sizeof(char));
+	print("Size of an int. 4 byte(s)\n", sizeof(int));
+	print("Size of a long int.4 byte(s)\n", sizeof(long int));
+	print("Size of a long long int.8 byte(s)\n", sizeof(long long int));
+	print("Size of a float. 4 byte(s)\n", sizeof(float)); 
 	return (0); 
 } 
 
